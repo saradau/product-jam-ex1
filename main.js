2d5372cfb7ab722dbed59e226a36f8e8f177d3b8
saradauksz <sara.dauksz@mail.huji.ac.il>
2024-11-06 13:16:53 +0200

    Everything looks good

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
-console.log("Hello World!");
-setupCounter();
+// JavaScript to toggle the active class on the profile pic
+document.addEventListener("DOMContentLoaded", function () {
+  const profilePic = document.getElementById("profile-pic");
 
-function setupCounter() {
-  let count = 0;
+  profilePic.addEventListener("mouseenter", function () {
+    profilePic.classList.add("animate-image");
+  });
 
-  function increment() {
-    count++;
-    document.querySelector("#count").innerHTML = count;
+  profilePic.addEventListener("mouseleave", function () {
+    profilePic.classList.remove("animate-image");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Check if the current page is the main page
+  if (document.body.classList.contains("main-page")) {
+    const bubbleCount = 20;
+    const maxTimeBetweenBubbles = 1600;
+    let timeBetweenBubbles = maxTimeBetweenBubbles;
+    let count = 0;
+
+    createBubble();
+
+    const interval = setInterval(() => {
+      createBubble();
+      count++;
+
+      if (count === bubbleCount) {
+        clearInterval(interval);
+      }
+
+      timeBetweenBubbles = Math.random() * maxTimeBetweenBubbles
+    }, timeBetweenBubbles);
+
+    function createBubble() {
+      const bubble = document.createElement("div");
+      bubble.classList.add("bubble");
+
+      // Set initial random properties
+      bubble.style.width = `${Math.random() * 60 + 20}px`;
+      bubble.style.height = bubble.style.width;
+      bubble.style.left = `${Math.random() * 90}%`;
+      bubble.style.bottom = `${Math.random() * 20}px`;
+      bubble.style.animationDuration = `${Math.random() * 30 + 30}s`;
+      document.body.appendChild(bubble);
+    }
   }
+});
 
-  function decrement() {
-    count--;
-    document.querySelector("#count").innerHTML = count;
-  }
 
-  document.querySelector("#increment").addEventListener("click", increment);
-  document.querySelector("#decrement").addEventListener("click", decrement);
-}
+
+
+
+
+
+
